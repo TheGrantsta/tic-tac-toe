3bf05f005a9e43946da6e6a35509a5edbe846574
Grant McKenna <grant.mckenna@madetech.com>
2020-10-16 12:09:52 +0100

    Amend winner to check if middle row is winning combination

--- winner.rb
+++ winner.rb
@@ -1,9 +1,26 @@
 class Winner
   WINNING_COMBINATION = "X-X-X"
 
-  def self.is_win combinations
-    top_row = combinations.slice(0, 3).join("-")
-
-    top_row == WINNING_COMBINATION
+  def initialize combinations
+    @combinations = combinations
   end
+
+  def is_win
+    if is_top_row
+      true
+    elsif is_middle_row
+      true
+    else
+      false
+    end
+  end
+
+  private
+    def is_top_row
+      @combinations.slice(0, 3).join("-") == WINNING_COMBINATION
+    end
+
+    def is_middle_row
+      @combinations.slice(3, 3).join("-") == WINNING_COMBINATION
+    end
 end
--- winner_spec.rb
+++ winner_spec.rb
@@ -2,18 +2,44 @@ require_relative "winner"
 
 RSpec.describe Winner do
   describe "winning combinationss" do
+    before do
+      @grid = Array.new(9, "")
+    end
     it "should return true for top row" do
-      winner = Winner.is_win ["X", "X", "X"]
+      @grid[0] = "X"; @grid[1] = "X"; @grid[2] = "X"
 
-      expect(winner).to be_truthy
+      winner = Winner.new @grid
+
+      expect(winner.is_win).to be_truthy
+    end
+
+    it "should return true for middle row" do
+      @grid[3] = "X"; @grid[4] = "X"; @grid[5] = "X"
+
+      winner = Winner.new @grid
+
+      expect(winner.is_win).to be_truthy
     end
   end
-  
-  describe "not winning combinationss" do
-    it "should return false for top row" do
-      winner = Winner.is_win ["X", "", "X"]
 
-      expect(winner).to be_falsey
+  describe "not winning combinationss" do
+    before do
+      @grid = ["", "", "", "", "", "", "", "", "", ""]
+    end
+    it "should return false for top row" do
+      @grid[0] = "X"; @grid[1] = "O"; @grid[2] = "X"
+
+      winner = Winner.new @grid
+
+      expect(winner.is_win).to be_falsey
+    end
+
+    it "should return false for middle row" do
+      @grid[3] = "X"; @grid[4] = "O"; @grid[5] = "X"
+
+      winner = Winner.new @grid
+
+      expect(winner.is_win).to be_falsey
     end
   end
 end
