5e971b66fb3c65557e0e0919f67329fcd15c203a
Grant McKenna <grant.mckenna@madetech.com>
2020-10-16 13:01:46 +0100

    Refactor: replace checker for each column and row with iterative checks

--- winner.rb
+++ winner.rb
@@ -7,53 +7,47 @@ class Winner
 
   def is_win
     is_winner = false
-
-    array_of_checkers = [
-      method(:is_top_row_match), method(:is_middle_row_match), method(:is_bottom_row_match),
-      method(:is_first_column_match), method(:is_second_column_match), method(:is_third_column_match),
-      method(:is_left_to_right_diagonal_match), method(:is_right_to_left_diagonal_match)
-    ]
-
-    array_of_checkers.each do |checker|
+    [method(:is_across_match), method(:is_down_match), method(:is_diagonal_match)].each do |checker|
       if checker.call
         is_winner = true
+        break
       end
     end
-
     is_winner
   end
 
   private
-    def is_top_row_match
-      is_match @combinations.slice(0, 3).join("-")
+    def is_across_match
+      is_across_win = false
+      [0,3,6].each do |c|
+        if is_match(@combinations.slice(c, 3).join("-"))
+          is_across_win = true
+          break
+        end
+      end
+      is_across_win
     end
 
-    def is_middle_row_match
-      is_match @combinations.slice(3, 3).join("-")
+    def is_down_match
+      is_down_win = false
+      [0,1,2].each do |c|
+        if is_match("#{@combinations[c]}-#{@combinations[c + 3]}-#{@combinations[c + 6]}")
+          is_down_win = true
+          break
+        end
+      end
+      is_down_win
     end
 
-    def is_bottom_row_match
-      is_match @combinations.slice(6, 3).join("-")
-    end
-
-    def is_first_column_match
-      is_match "#{@combinations[0]}-#{@combinations[3]}-#{@combinations[6]}"
-    end
-
-    def is_second_column_match
-      is_match "#{@combinations[1]}-#{@combinations[4]}-#{@combinations[7]}"
-    end
-
-    def is_third_column_match
-      is_match "#{@combinations[2]}-#{@combinations[5]}-#{@combinations[8]}"
-    end
-
-    def is_left_to_right_diagonal_match
-      is_match "#{@combinations[0]}-#{@combinations[4]}-#{@combinations[8]}"
-    end
-
-    def is_right_to_left_diagonal_match
-      is_match "#{@combinations[2]}-#{@combinations[4]}-#{@combinations[6]}"
+    def is_diagonal_match
+      is_diagonal_win = false
+      [0,2].each do |c|
+        if is_match("#{@combinations[(8 - c).abs]}-#{@combinations[4]}-#{@combinations[c]}")
+          is_diagonal_win = true
+          break
+        end
+      end
+      is_diagonal_win
     end
 
     def is_match input
