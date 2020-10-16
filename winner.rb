91d0b8713818078ab2e1065f1d05676b8b35faa1
Grant McKenna <grant.mckenna@madetech.com>
2020-10-16 12:43:26 +0100

    Refactor: remove multiple comparisons

--- winner.rb
+++ winner.rb
@@ -11,7 +11,7 @@ class Winner
     array_of_checkers = [
       method(:is_top_row_match), method(:is_middle_row_match), method(:is_bottom_row_match),
       method(:is_first_column_match), method(:is_second_column_match), method(:is_third_column_match),
-      method(:is_diagonal_match)
+      method(:is_left_to_right_diagonal_match), method(:is_right_to_left_diagonal_match)
     ]
 
     array_of_checkers.each do |checker|
@@ -25,33 +25,38 @@ class Winner
 
   private
     def is_top_row_match
-      @combinations.slice(0, 3).join("-") == WINNING_COMBINATION
+      is_match @combinations.slice(0, 3).join("-")
     end
 
     def is_middle_row_match
-      @combinations.slice(3, 3).join("-") == WINNING_COMBINATION
+      is_match @combinations.slice(3, 3).join("-")
     end
 
     def is_bottom_row_match
-      @combinations.slice(6, 3).join("-") == WINNING_COMBINATION
+      is_match @combinations.slice(6, 3).join("-")
     end
 
     def is_first_column_match
-      "#{@combinations[0]}-#{@combinations[3]}-#{@combinations[6]}" == WINNING_COMBINATION
+      is_match "#{@combinations[0]}-#{@combinations[3]}-#{@combinations[6]}"
     end
 
     def is_second_column_match
-      "#{@combinations[1]}-#{@combinations[4]}-#{@combinations[7]}" == WINNING_COMBINATION
+      is_match "#{@combinations[1]}-#{@combinations[4]}-#{@combinations[7]}"
     end
 
     def is_third_column_match
-      "#{@combinations[2]}-#{@combinations[5]}-#{@combinations[8]}" == WINNING_COMBINATION
+      is_match "#{@combinations[2]}-#{@combinations[5]}-#{@combinations[8]}"
     end
 
-    def is_diagonal_match
-      left_to_right = "#{@combinations[0]}-#{@combinations[4]}-#{@combinations[8]}"
-      right_to_left = "#{@combinations[2]}-#{@combinations[4]}-#{@combinations[6]}"
+    def is_left_to_right_diagonal_match
+      is_match "#{@combinations[0]}-#{@combinations[4]}-#{@combinations[8]}"
+    end
 
-      left_to_right == WINNING_COMBINATION || right_to_left == WINNING_COMBINATION
+    def is_right_to_left_diagonal_match
+      is_match "#{@combinations[2]}-#{@combinations[4]}-#{@combinations[6]}"
+    end
+
+    def is_match input
+      input == WINNING_COMBINATION
     end
 end
