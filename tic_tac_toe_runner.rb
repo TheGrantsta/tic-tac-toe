a4ce11987ac293267c83908bef4c30edb14e200a
Grant McKenna <grant.mckenna@madetech.com>
2020-10-17 07:40:32 +0100

    Amend tic tac toe runner to format display

--- tic_tac_toe_runner.rb
+++ tic_tac_toe_runner.rb
@@ -2,18 +2,16 @@ require_relative "game"
 
 @game = Game.new [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-puts "*************************"
-
-puts @game.display_grid
-puts
-
 while @game.is_playing do
+  puts "*************************"
+  puts @game.display_grid
+  puts
   puts @game.available_squares
+  puts
+  puts "*************************"
+
   response = $stdin.gets.chomp
 
   @game.add_player_choice response.to_i
-  puts
-  puts @game.display_grid
-end
 
-puts "*************************"
+end
