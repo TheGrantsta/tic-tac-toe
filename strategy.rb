7b5f375ace670c8ebe633d77ad24e113e1401284
Grant McKenna <grant.mckenna@madetech.com>
2020-10-19 15:08:19 +0100

    Refactor: remove duplication for finding next move

--- strategy.rb
+++ strategy.rb
@@ -1,5 +1,7 @@
 class Strategy
   NO_SELECTION = 0
+  PLAYER_MARK = "X"
+  COMPUTER_MARK = "O"
 
   def self.move combinations
     selection = computer_turn combinations
@@ -30,91 +32,79 @@ class Strategy
   end
 
   def self.get_winning_selection combinations
-    square = is_across_win combinations
+    square = is_across_win combinations, COMPUTER_MARK, PLAYER_MARK
 
-    square = is_down_win combinations unless square > 0
+    square = is_down_win combinations, COMPUTER_MARK, PLAYER_MARK unless square > NO_SELECTION
 
-    square = is_diagonal_win combinations unless square > 0
+    square = is_diagonal_win combinations, COMPUTER_MARK, PLAYER_MARK unless square > NO_SELECTION
 
     square
   end
 
   def self.get_blocking_selection combinations
-    square = is_across_block combinations
+    square = is_across_block combinations, PLAYER_MARK, COMPUTER_MARK
 
-    square = is_down_block combinations unless square > 0
+    square = is_down_block combinations, PLAYER_MARK, COMPUTER_MARK unless square > NO_SELECTION
 
-    square = is_diagonal_block combinations unless square > 0
+    square = is_diagonal_block combinations, PLAYER_MARK, COMPUTER_MARK unless square > NO_SELECTION
 
     square
   end
 
-  def self.is_across_win combinations
+  def self.is_across_win combinations, twoOf, noneOf
+    is_across combinations, twoOf, noneOf
+  end
+
+  def self.is_across_block combinations, twoOf, noneOf
+    is_across combinations, twoOf, noneOf
+  end
+
+  def self.is_across combinations, twoOf, noneOf
     [0,3,6].each do |c|
       options = combinations.slice(c, 3)
-      if options.filter { |o| o == "O" }.count == 2 && options.filter { |o| o == "X" }.count == 0
+      if options.filter { |o| o == twoOf }.count == 2 && options.filter { |o| o == noneOf }.count == 0
         return combinations.slice(c, 3).filter { |o| o.is_a? Integer }[0]
       end
     end
     NO_SELECTION
   end
 
-  def self.is_down_win combinations
+  def self.is_down_win combinations, twoOf, noneOf
+    is_down combinations, twoOf, noneOf
+  end
+
+  def self.is_down_block combinations, twoOf, noneOf
+    is_down combinations, twoOf, noneOf
+  end
+
+  def self.is_down combinations, twoOf, noneOf
     [0,1,2].each do |c|
       options = []
       options.push combinations[c]
       options.push combinations[c + 3]
       options.push combinations[c + 6]
-      if options.filter { |o| o == "O" }.count == 2 && options.filter { |o| o == "X" }.count == 0
+      if options.filter { |o| o == twoOf }.count == 2 && options.filter { |o| o == noneOf }.count == 0
         return options.filter { |o| o.is_a? Integer }[0]
       end
     end
     NO_SELECTION
   end
 
-  def self.is_diagonal_win combinations
+  def self.is_diagonal_win combinations, twoOf, noneOf
+    is_diagonal combinations, twoOf, noneOf
+  end
+
+  def self.is_diagonal_block combinations, twoOf, noneOf
+    is_diagonal combinations, twoOf, noneOf
+  end
+
+  def self.is_diagonal combinations, twoOf, noneOf
     [0,2].each do |c|
       options = []
       options.push combinations[8 - c]
       options.push combinations[4]
       options.push combinations[c]
-      if options.filter { |o| o == "O" }.count == 2 && options.filter { |o| o == "X" }.count == 0
-        return options.filter { |o| o.is_a? Integer }[0]
-      end
-    end
-    NO_SELECTION
-  end
-
-  def self.is_across_block combinations
-    [0,3,6].each do |c|
-      options = combinations.slice(c, 3)
-      if options.filter { |o| o == "X" }.count == 2 && options.filter { |o| o == "O" }.count == 0
-        return combinations.slice(c, 3).filter { |o| o.is_a? Integer }[0]
-      end
-    end
-    NO_SELECTION
-  end
-
-  def self.is_down_block combinations
-    [0,1,2].each do |c|
-      options = []
-      options.push combinations[c]
-      options.push combinations[c + 3]
-      options.push combinations[c + 6]
-      if options.filter { |o| o == "X" }.count == 2 && options.filter { |o| o == "O" }.count == 0
-        return options.filter { |o| o.is_a? Integer }[0]
-      end
-    end
-    NO_SELECTION
-  end
-
-  def self.is_diagonal_block combinations
-    [0,2].each do |c|
-      options = []
-      options.push combinations[8 - c]
-      options.push combinations[4]
-      options.push combinations[c]
-      if options.filter { |o| o == "X" }.count == 2 && options.filter { |o| o == "O" }.count == 0
+      if options.filter { |o| o == twoOf }.count == 2 && options.filter { |o| o == noneOf }.count == 0
         return options.filter { |o| o.is_a? Integer }[0]
       end
     end
